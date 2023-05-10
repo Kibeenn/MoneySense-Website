54a18437840cbf9defec59147ecc676cc9978f7f
Kibeenn <quimba.ninokevin.bscs2020@gmail.com>
2023-05-11 00:38:09 +0800

    2:30:00

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,43 +1,45 @@
-import React, {useState, useMemo} from 'react'
+import React, { useState, useMemo } from "react";
 import styled from "styled-components";
-import bg from './img/bg.png'
-import {MainLayout} from './styles/Layouts'
-import Orb from './Components/Orb/Orb'
-import Navigation from './Components/Navigation/Navigation'
+import bg from "./img/bg.png";
+import { MainLayout } from "./styles/Layouts";
+import Orb from "./Components/Orb/Orb";
+import Navigation from "./Components/Navigation/Navigation";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Income from "./Components/Incomes/Incomes";
 import Expenses from "./Components/Expenses/Expenses";
+import { useGlobalContext } from "./context/globalContext";
 
 function App() {
-  const [active, setActive] = useState(1)
+  const [active, setActive] = useState(1);
 
-    const displayData = () => {
-      switch (active){
-        case 1:
-          return <Dashboard />
-        case 2:
-          return <Dashboard />
-        case 3:
-          return <Income />
-        case 4:
-          return <Expenses />
-        default: 
-          return <Dashboard />
-      }
+  const global = useGlobalContext();
+  console.log(global);
+
+  const displayData = () => {
+    switch (active) {
+      case 1:
+        return <Dashboard />;
+      case 2:
+        return <Dashboard />;
+      case 3:
+        return <Income />;
+      case 4:
+        return <Expenses />;
+      default:
+        return <Dashboard />;
     }
-  
-    const orbMemo = useMemo(() => {
-      return <Orb />
-    },[])
+  };
+
+  const orbMemo = useMemo(() => {
+    return <Orb />;
+  }, []);
 
   return (
     <AppStyled bg={bg} className="App">
       {orbMemo}
       <MainLayout>
-          <Navigation active={active} setActive={setActive} />
-            <main>
-              {displayData()}
-            </main>
+        <Navigation active={active} setActive={setActive} />
+        <main>{displayData()}</main>
       </MainLayout>
     </AppStyled>
   );
@@ -45,21 +47,19 @@ function App() {
 
 const AppStyled = styled.div`
   height: 100vh;
-  background-image: url(${props => props.bg});
+  background-image: url(${(props) => props.bg});
   position: relative;
-  main{
+  main {
     flex: 1;
     background: rgba(252, 246, 249, 0.78);
-    border: 3px solid #FFFFFF;
+    border: 3px solid #ffffff;
     backdrop-filter: blur(4.5px);
     border-radius: 32px;
     overflow-x: hidden;
-    &::-webkit-scrollbar{
+    &::-webkit-scrollbar {
       width: 0;
     }
   }
 `;
-
-
 
 export default App;
--- Frontend/src/Components/Incomes/Incomes.js
+++ Frontend/src/Components/Incomes/Incomes.js
@@ -1,19 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import { InnerLayout } from "../../styles/Layouts";
+import { useGlobalContext } from "../../context/globalContext";
 
 function Income() {
-    return (
-        <IncomeStyled>
-            <InnerLayout>
-                
-            </InnerLayout>
-        </IncomeStyled>
-    )
+  const { addIncome } = useGlobalContext();
+  return (
+    <IncomeStyled>
+      <InnerLayout>
+        <h1>Incomes</h1>
+        <div className="income-content">
+          <div className="from-container"></div>
+          <div className="incomes"></div>
+        </div>
+      </InnerLayout>
+    </IncomeStyled>
+  );
 }
 
-const IncomeStyled = styled.div`
+const IncomeStyled = styled.div``;
 
-`;
-
-export default Income
+export default Income;
