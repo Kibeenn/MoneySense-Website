15c62fde6fc20cea4883829657a67df665fc3f59
Kibeenn <quimba.ninokevin.bscs2020@gmail.com>
2023-05-11 13:52:02 +0800

    Removed unnecessary

--- Frontend/src/Components/Navigation/Navigation.js
+++ Frontend/src/Components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import kevin from "../../img/kevin.jpg";
 import { signout } from "../../utils/Icons";
